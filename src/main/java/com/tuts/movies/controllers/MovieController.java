17707bf35b9aed713e7b05a71d553a8d520496e1
ywalakamar <barxwells@gmail.com>
2023-06-11 11:04:33 +0300

    feat: enable cross origin access using annotations

--- src/main/java/com/tuts/movies/controllers/MovieController.java
+++ src/main/java/com/tuts/movies/controllers/MovieController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tuts.movies.models.Movie;
 import com.tuts.movies.services.MovieService;
 
+@CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
 @RequestMapping(value = "/api/v1/movies")
 public class MovieController {
--- src/main/java/com/tuts/movies/controllers/ReviewController.java
+++ src/main/java/com/tuts/movies/controllers/ReviewController.java
@@ -3,6 +3,7 @@ package com.tuts.movies.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.tuts.movies.services.ReviewService;
 
 import java.util.Map;
 
+@CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
 @RequestMapping(value = "/api/v1/reviews")
 public class ReviewController {
